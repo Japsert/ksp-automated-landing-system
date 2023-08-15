ff84486d953f39db58eb9dcee07938a9305c67ce
Japsert <40762304+Japsert@users.noreply.github.com>
2023-08-15 23:09:17 +0200

    Add support for plotting runs simultaneously

--- impact/plot_impact.py
+++ impact/plot_impact.py
@@ -7,31 +7,53 @@ import numpy as np
 from math import pi
 from time import time
 import matplotlib.pyplot as plt
-import pandas as pd
+import matplotlib as mpl
 from matplotlib.animation import FuncAnimation
 
 # Configuration
 speedup_factor = 10
 zoom_factor = 1
+run_colors = ["blue", "green", "orange", "purple", "brown", "pink", "gray"]
+
+mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=run_colors) # set run colors
 
 # Read data
-data = pd.read_csv("impact/impact.log")
-times = data["time"].values
-lats = data["lat"].values
-lngs = data["lng"].values
+log_file = "impact/impact.log"
+with open(log_file, "r") as f:
+    lines = f.readlines()[1:]  # skip header
+
+runs = []
+
+time_col = 0
+lat_col = 1
+lng_col = 2
+
+for line in lines:
+    if line.strip() == "---":
+        # start a new run
+        runs.append([])
+        continue
+    # add data to current run
+    cols = line.split(",")
+    runs[-1].append((float(cols[time_col]), float(cols[lat_col]), float(cols[lng_col])))
+
 start = time()
-data_start_time = times[0]
-log_time_range = times[-1] - times[0]
+data_start_time = min(run[0][time_col] for run in runs)
+log_time_range = max(run[-1][time_col] for run in runs) - data_start_time
 
 fig, ax = plt.subplots()
 
 # Calculate center and bounds
-center_lat = lats[-1]
-center_lng = lngs[-1]
+center_lat = runs[0][-1][lat_col]  # take first run's last point as center
+center_lng = runs[0][-1][lng_col]
 
 # Calculate x and y limits
-# get the maximum deviation of any data points from the center
-max_deviation = max(max(abs(lats - center_lat)), max(abs(lngs - center_lng)))
+# get the maximum deviation of any data points of any run from the center
+max_deviation = max(
+    max(abs(point[lat_col] - center_lat), abs(point[lng_col] - center_lng))
+    for run in runs
+    for point in run
+)
 # use that, plus a little wiggle room, to calculate the bounds
 lim_offset = max_deviation * 1.1 / zoom_factor
 xmax = center_lng + lim_offset
@@ -61,21 +83,32 @@ ax.set_yticks(yticks, labels=yticklabels)
 ax.set_xlabel("Longitude (m)")
 ax.set_ylabel("Latitude (m)")
 
-(line,) = ax.plot([], [])
-(newest_point,) = ax.plot([], [], "rx", markersize=7)
+lines = []
+newest_points = []
+# Plot runs (empty for now)
+for run in runs:
+    (line,) = ax.plot([], [])
+    (newest_point,) = ax.plot([], [], "rx", markersize=7)
+    lines.append(line)
+    newest_points.append(newest_point)
+
+ax.legend(lines, [f"Run {i}" for i in range(len(runs))])
 
 
 def get_values_until_time(time):
-    values = []
-    for i, t in enumerate(times):
-        if data_start_time + time > t:
-            values.append((lngs[i], lats[i]))
-        else:
-            return values
+    # Return list of runs, containing lists of points, until the given time
+    return_runs = []
+    for run in runs:
+        return_runs.append([])
+        for point in run:
+            if data_start_time + time >= point[time_col]:
+                return_runs[-1].append(point)
+            else:
+                break
+    return return_runs
 
 
 def update(frame):
-    # loop through the data until the time is after the current time
     real_time_since_start = time() - start
     sped_up_time = real_time_since_start * speedup_factor
 
@@ -84,17 +117,21 @@ def update(frame):
         animation.event_source.stop()
         return
 
-    points_to_plot = get_values_until_time(sped_up_time)
-    if points_to_plot:
-        xdata, ydata = zip(*points_to_plot)
-        line.set_data(xdata, ydata)
-        # plot the newest point in red
-        newest_point.set_data([xdata[-1]], [ydata[-1]])
+    runs = get_values_until_time(sped_up_time)
+    for run, line, newest_point in zip(runs, lines, newest_points):
+        if len(run) == 0:
+            continue
+        line.set_data(
+            [point[lng_col] for point in run], [point[lat_col] for point in run]
+        )
+        # plot newest points as red crosses
+        newest_point.set_data(run[-1][lng_col], run[-1][lat_col])
 
     ax.set_title(
         f"{sped_up_time:.1f}/{log_time_range:.1f} seconds (sped up {speedup_factor}x)"
     )
-    return line, newest_point
+
+    return line, newest_points
 
 
 animation = FuncAnimation(fig=fig, func=update, interval=1, cache_frame_data=False)
