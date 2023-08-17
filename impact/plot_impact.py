b3926e9eaf66f32d0108b7f97fe217825668e852
Japsert <40762304+Japsert@users.noreply.github.com>
2023-08-17 16:48:46 +0200

    Add support for naming pred. runs

--- impact/plot_impact.py
+++ impact/plot_impact.py
@@ -3,6 +3,7 @@
 # - y axis is latitude
 # - plot is animated over time
 
+import re
 import numpy as np
 from math import pi
 from time import time
@@ -15,7 +16,7 @@ speedup_factor = 10
 zoom_factor = 1
 run_colors = ["blue", "green", "orange", "purple", "brown", "pink", "gray"]
 
-mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=run_colors) # set run colors
+mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=run_colors)  # set run colors
 
 # Read data
 log_file = "impact/impact.log"
@@ -29,30 +30,32 @@ lat_col = 1
 lng_col = 2
 
 for line in lines:
-    if line.strip() == "---":
+    if line.startswith("---"):
         # start a new run
-        runs.append([])
+        runs.append({"name": re.search(r"--- (.*)", line).group(1), "points": []})
         continue
     # add data to current run
     cols = line.split(",")
-    runs[-1].append((float(cols[time_col]), float(cols[lat_col]), float(cols[lng_col])))
+    runs[-1]["points"].append(
+        (float(cols[time_col]), float(cols[lat_col]), float(cols[lng_col]))
+    )
 
 start = time()
-data_start_time = min(run[0][time_col] for run in runs)
-log_time_range = max(run[-1][time_col] for run in runs) - data_start_time
+data_start_time = min(run["points"][0][time_col] for run in runs)
+log_time_range = max(run["points"][-1][time_col] for run in runs) - data_start_time
 
 fig, ax = plt.subplots()
 
 # Calculate center and bounds
-center_lat = runs[0][-1][lat_col]  # take first run's last point as center
-center_lng = runs[0][-1][lng_col]
+center_lat = runs[0]["points"][-1][lat_col]  # take first run's last point as center
+center_lng = runs[0]["points"][-1][lng_col]
 
 # Calculate x and y limits
 # get the maximum deviation of any data points of any run from the center
 max_deviation = max(
     max(abs(point[lat_col] - center_lat), abs(point[lng_col] - center_lng))
     for run in runs
-    for point in run
+    for point in run["points"]
 )
 # use that, plus a little wiggle room, to calculate the bounds
 lim_offset = max_deviation * 1.1 / zoom_factor
@@ -92,15 +95,16 @@ for run in runs:
     lines.append(line)
     newest_points.append(newest_point)
 
-ax.legend(lines, [f"Run {i}" for i in range(len(runs))])
+ax.legend(lines, [run["name"] for run in runs])
 
 
 def get_values_until_time(time):
-    # Return list of runs, containing lists of points, until the given time
+    # Return list of runs, containing lists of point tuples,
+    # that are before the given time
     return_runs = []
     for run in runs:
         return_runs.append([])
-        for point in run:
+        for point in run["points"]:
             if data_start_time + time >= point[time_col]:
                 return_runs[-1].append(point)
             else:
@@ -122,13 +126,15 @@ def update(frame):
         if len(run) == 0:
             continue
         line.set_data(
-            [point[lng_col] for point in run], [point[lat_col] for point in run]
+            [point[lng_col] for point in run],
+            [point[lat_col] for point in run],
         )
         # plot newest points as red crosses
         newest_point.set_data([run[-1][lng_col]], [run[-1][lat_col]])
 
     ax.set_title(
-        f"{sped_up_time:.1f}/{log_time_range:.1f} seconds (sped up {speedup_factor}x)"
+        f"{sped_up_time:.1f}/{log_time_range:.1f} seconds "
+        "(sped up {speedup_factor}x)"
     )
 
     return line, newest_points
